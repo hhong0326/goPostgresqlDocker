c408de0a113adaa2399c2b3460a8ad83a1dac456
hhong0326 <hhong0326@naver.com>
2022-03-05 09:06:47 +0900

    Avoid Deadlock where tx

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -58,10 +58,7 @@ func TestUpdateAccount(t *testing.T) {
 		Balance: util.RandomMoney(),
 	}
 
-	err := testQueries.UpdateAccount(context.Background(), arg)
-	require.NoError(t, err)
-
-	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, account2)
 
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -96,10 +96,65 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		// TODO: update accounts' balance
+		// update accounts' balance
+		if arg.FromAccountID < arg.ToAccountID {
+
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+
+			// version 1
+			// account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
+			// if err != nil {
+			// 	return err
+			// }
+
+			// account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
+			// if err != nil {
+			// 	return err
+			// }
+
+			// version 2
+			// result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			// 	ID:     arg.FromAccountID,
+			// 	Amount: -arg.Amount,
+			// })
+			// if err != nil {
+			// 	return err
+			// }
+
+			// result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			// 	ID:     arg.ToAccountID,
+			// 	Amount: arg.Amount,
+			// })
+			// if err != nil {
+			// 	return err
+			// }
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+
+		}
 
 		return nil
 	})
 
 	return result, err
+}
+
+func addMoney(ctx context.Context, q *Queries, accountID1, amount1, accountID2, amount2 int64) (account1, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return // return account1, account2, err
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	if err != nil {
+		return
+	}
+
+	return
 }
