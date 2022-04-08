50173a109c3985705a939ee5dc01ec84d414ba0b
hhong0326 <hhong0326@naver.com>
2022-04-08 19:09:58 +0900

    Test tranfer api 1/?

--- api/transfer.go
+++ api/transfer.go
@@ -32,7 +32,7 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		return
 	}
 
-	authPayload := ctx.MustGet(authorizationHeaderKey).(*token.Payload)
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	if fromAccount.Owner != authPayload.Username {
 		err := errors.New("from account dosen't belong to the authenticated user")
 		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
--- api/transfer_test.go
+++ api/transfer_test.go
@@ -1,7 +1,70 @@
 package api
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
 
-func TestCreateTransfer(t *testing.T) {
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	mockdb "github.com/hhong0326/goPostgresqlDocker.git/db/mock"
+	db "github.com/hhong0326/goPostgresqlDocker.git/db/sqlc"
+	"github.com/hhong0326/goPostgresqlDocker.git/token"
+	"github.com/stretchr/testify/require"
+)
 
+func TestTransferAPI(t *testing.T) {
+
+	amount := int64(10)
+
+	user1, _ := randomUser(t)
+	user2, _ := randomUser(t)
+
+	account1 := randomAccount(user1.Username)
+	account2 := randomAccount(user2.Username)
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"from_account_id": account1.ID,
+				"to_account_id":   account2.ID,
+				"amount":          amount,
+				"currency":        "USD",
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user1.Username, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(account2, nil)
+
+				arg := db.TransferTxParams{
+					FromAccountID: account1.ID,
+					ToAccountID:   account2.ID,
+					Amount:        amount,
+				}
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Eq(arg)).Times(1)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+
+		})
+	}
 }
